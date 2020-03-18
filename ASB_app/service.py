e2466fedec9f2a56b6697b05233b372f23ad8443
M1kss <boytsovs.av@phystech.edu>
2020-03-18 13:48:25 +0300

    fixes

--- ASB_app/service.py
+++ ASB_app/service.py
@@ -49,7 +49,8 @@ def get_snps_by_advanced_filters(filters_object):
 
 def get_hints(what_for, in_str, used_options):
     cls = {'TF': TranscriptionFactor, 'CL': CellLine}[what_for]
-    filters = ((cls.name.like(in_str),) * bool(in_str) +
-               (not_(cls.name.in_(used_options)),) * bool(used_options))
+    filters = ((cls.name.like(in_str),) if in_str else () +
+               (not_(cls.name.in_(used_options)),) if used_options else ())
+    print(filters)
     return [entity.name for entity in session.query(cls.name).filter(*filters)
             .order_by(cls.aggregated_snps_count.desc()).limit(3).all()]
