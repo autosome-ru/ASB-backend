0e57f9814f03ffccc5d2de783b9b5c3b2d794004
M1kss <boytsovs.av@phystech.edu>
2020-03-29 20:25:17 +0300

    fixes

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
+dir = pathlib.Path(__file__).parent.absolute()
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.expanduser('~/AD_ASTRA_database/AD_ASTRA.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dir, 'AD_ASTRA.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESTPLUS_VALIDATE = True
     LOGGER_LEVEL = 'DEBUG'
