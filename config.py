682f43939098f50ff102a5bf43a1d36e98d16799
m1kss <boytsovs.av@phystech.edu>
2020-10-29 22:19:57 +0300

    rename test release to beta

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dir, 'AD_ASTRA.db')
     SQLALCHEMY_BINDS = {
         'soos': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA.db'),
-        'test': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_test.db'),
+        'beta': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_test.db'),
         'candidates': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_candidates.db'),
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
