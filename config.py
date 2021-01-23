db93caca4d6bb9154397695cbee9c8a6c1be1ee7
Sashok <boytsovs@mail.ru>
2021-01-23 21:51:36 +0300

    create database

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config(object):
     SQLALCHEMY_BINDS = {
         # 'soos': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_soos.db'),
         # 'ford': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_ford.db'),
-        'candidates': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_candidates.db'),
+        # 'candidates': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_candidates.db'),
         # 'tickets': 'sqlite:///' + os.path.join(dir, 'AD_ASTRA_tickets.db'),
         'soos': 'mysql://adastra:adastrapass@localhost:3306/adastra_soos?charset=utf8',
         'ford': 'mysql://adastra:adastrapass@localhost:3306/adastra_ford?charset=utf8',
-        # 'candidates': 'mysql://adastra:adastrapass@localhost:3306/adastra_candidates_soos?charset=utf8',
+        'candidates': 'mysql://adastra:adastrapass@localhost:3306/adastra_candidates_soos?charset=utf8',
         'tickets': 'mysql://adastra:adastrapass@localhost:3306/adastra_tickets?charset=utf8',
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,5 +22,5 @@ class Config(object):
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
     EXECUTOR_MAX_WORKERS = 1
     RAW_MYSQL_USERNAME = 'adastra'
-    RAW_MYSQL_PASSWORD = 'adastrapass'
+    RAW_MYSQL_PASSWORD = 'pass=ADASTRA880'
     RAW_MYSQL_HOSTNAME = 'localhost'
