9b2d5aeb50871436606d21e10a402eb6a96df4ed
Sashok <boytsovs@mail.ru>
2021-02-03 18:00:09 +0300

    database changes

--- create_database_from_release_ford.py
+++ create_database_from_release_ford.py
@@ -262,7 +262,9 @@ if __name__ == '__main__':
                 ag_id = ag.tf_id
 
             exp_snp = ExpSNP.query.filter(
-                getattr(ExpSNP, {'TF': 'tf_snp_id', 'CL': 'cl_snp_id'}[param]) == ag_snp_id,
+                getattr(ExpSNP, {'TF': 'transcription_factor_snp', 'CL': 'cell_line_snp'}[param]).has(
+                    getattr(ExpSNP, {'TF': 'tf_id', 'CL': 'cl_id'}[param]) == ag_id,
+                ),
             ).first()
             if exp_snp:
                 continue
