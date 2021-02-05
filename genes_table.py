9d2bdea2b4c9367bc8babf8a974835a0cfe5cd61
Sashok <boytsovs@mail.ru>
2021-02-05 17:19:33 +0300

    database changes

--- genes_table.py
+++ genes_table.py
@@ -8,8 +8,12 @@ Gene = release.Gene
 SNP = release.SNP
 TFSNP = release.TranscriptionFactorSNP
 TF = release.TranscriptionFactor
+CL = release.CellLine
+ExpSNP = release.ExpSNP
+Experiment = release.Experiment
 
 session = release.session
+db = release.db
 
 
 def get_filters_by_gene(self, gene):
@@ -20,12 +24,43 @@ def get_filters_by_gene(self, gene):
         return self.SNP.chromosome == gene.chromosome, self.SNP.position.between(max(gene.start_pos, 1),
                                                                                  gene.end_pos + 5000)
 
-q = session.query(Gene, SNP, TFSNP, TF)\
-    .join(SNP, Gene.snps_by_target)\
-    .join(TFSNP, SNP.tf_aggregated_snps)\
-    .filter(TFSNP.best_p_value > np.log10(20))\
-    .join(TF, TFSNP.transcription_factor)\
-    .limit(10)
 
-for (gene, snp, tfsnp, tf) in q:
-    print(gene.gene_name, snp.rs_id, tfsnp.log_p_value_ref, tf.name)
+q = session.query(
+        Gene,
+        SNP,
+        TFSNP,
+        TF,
+        db.func.group_concat(db.func.distinct(CL.name), separator=', ')
+    ).join(
+        SNP,
+        Gene.snps_by_target
+    ).join(
+        TFSNP,
+        SNP.tf_aggregated_snps
+    ).filter(
+        TFSNP.best_p_value > np.log10(20)
+    ).join(
+        TF,
+        TFSNP.transcription_factor
+    ).join(
+        ExpSNP,
+        TFSNP.exp_snps
+    ).filter(
+        (ExpSNP.p_value_ref - ExpSNP.p_value_alt) * (
+                    TFSNP.log_p_value_alt - TFSNP.log_p_value_ref) > 0
+    ).join(
+        Experiment,
+        ExpSNP.experiment,
+    ).join(
+        CL,
+        Experiment.cell_line,
+    ).group_by(
+        Gene,
+        SNP,
+        TFSNP,
+        TF
+    ).limit(10)
+
+
+for (gene, snp, tfsnp, tf, cl_names) in q:
+    print(gene.gene_name, 'rs' + str(snp.rs_id), tfsnp.log_p_value_ref, tfsnp.log_p_value_ref, tf.name, cl_names)
