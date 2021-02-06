b660ca19921d2b3e9e7e957060520769b2cc4c60
Sashok <boytsovs@mail.ru>
2021-02-06 16:59:39 +0300

    debug

--- genes_table.py
+++ genes_table.py
@@ -72,6 +72,8 @@ if __name__ == '__main__':
             getattr(Experiment, 'transcription_factor' if AG == CL else 'cell_line')
         )
 
+    print(q_target)
+
     # q_promoter = session.query(
     #         Gene,
     #         SNP,
@@ -107,51 +109,51 @@ if __name__ == '__main__':
     #         getattr(Experiment, 'transcription_factor' if AG == CL else 'cell_line')
     #     )
 
-    q_promoter = []
-
-    promoter_dict = {}
-    target_dict = {}
-    for q, q_dict in (q_promoter, promoter_dict), (q_target, target_dict):
-        for (gene, snp, agsnp, ag, other_name) in q:
-            if gene.gene_id in q_dict:
-                q_dict[gene.gene_id][-1].add(other_name)
-            else:
-                q_dict[gene.gene_id] = [gene.gene_name, snp.chromosome, snp.position,
-                                        'rs' + str(snp.rs_id), snp.ref, snp.alt, ag.name,
-                                        '{} ({})'.format(*(('ref', snp.ref) if agsnp.log_p_value_ref > agsnp.log_p_value_alt else ('alt', snp.alt))),
-                                        max(agsnp.log_p_value_ref, agsnp.log_p_value_alt),
-                                        agsnp.es_ref if agsnp.log_p_value_ref > agsnp.log_p_value_alt else agsnp.es_alt,
-                                        {other_name}]
-
-    all_genes = list(set(promoter_dict.keys()) | set(target_dict.keys()))
-
-    with open(os.path.expanduser('~/{}_genes_all.tsv'.format('tf' if AG == TF else 'cl')), 'w') as out:
-        out.write(
-            '\t'.join(
-                map(str, [
-                    'Gene_name',
-                    'Chromosome',
-                    'Position'
-                    'rs_ID',
-                    'Ref',
-                    'Alt',
-                    'TF' if AG == TF else 'Cells',
-                    'Preferred_allele',
-                    'Log10_p_value',
-                    'Effect_size(log2)',
-                    'Supporting_{}'.format('TFs' if AG == CL else 'Cell_types'),
-                    'eQTL',
-                    'promoter_SNP',
-                ])
-            ) + '\n'
-        )
-        for gene_id in all_genes:
-            if gene_id in target_dict:
-                data = target_dict[gene_id]
-            else:
-                data = promoter_dict[gene_id]
-            out.write(
-                '\t'.join(
-                    map(str, data[:-1] + ['|'.join(data[-1])] + [gene_id in target_dict, gene_id in promoter_dict])
-                ) + '\n'
-            )
+    # q_promoter = []
+    #
+    # promoter_dict = {}
+    # target_dict = {}
+    # for q, q_dict in (q_promoter, promoter_dict), (q_target, target_dict):
+    #     for (gene, snp, agsnp, ag, other_name) in q:
+    #         if gene.gene_id in q_dict:
+    #             q_dict[gene.gene_id][-1].add(other_name)
+    #         else:
+    #             q_dict[gene.gene_id] = [gene.gene_name, snp.chromosome, snp.position,
+    #                                     'rs' + str(snp.rs_id), snp.ref, snp.alt, ag.name,
+    #                                     '{} ({})'.format(*(('ref', snp.ref) if agsnp.log_p_value_ref > agsnp.log_p_value_alt else ('alt', snp.alt))),
+    #                                     max(agsnp.log_p_value_ref, agsnp.log_p_value_alt),
+    #                                     agsnp.es_ref if agsnp.log_p_value_ref > agsnp.log_p_value_alt else agsnp.es_alt,
+    #                                     {other_name}]
+    #
+    # all_genes = list(set(promoter_dict.keys()) | set(target_dict.keys()))
+    #
+    # with open(os.path.expanduser('~/{}_genes_all.tsv'.format('tf' if AG == TF else 'cl')), 'w') as out:
+    #     out.write(
+    #         '\t'.join(
+    #             map(str, [
+    #                 'Gene_name',
+    #                 'Chromosome',
+    #                 'Position'
+    #                 'rs_ID',
+    #                 'Ref',
+    #                 'Alt',
+    #                 'TF' if AG == TF else 'Cells',
+    #                 'Preferred_allele',
+    #                 'Log10_p_value',
+    #                 'Effect_size(log2)',
+    #                 'Supporting_{}'.format('TFs' if AG == CL else 'Cell_types'),
+    #                 'eQTL',
+    #                 'promoter_SNP',
+    #             ])
+    #         ) + '\n'
+    #     )
+    #     for gene_id in all_genes:
+    #         if gene_id in target_dict:
+    #             data = target_dict[gene_id]
+    #         else:
+    #             data = promoter_dict[gene_id]
+    #         out.write(
+    #             '\t'.join(
+    #                 map(str, data[:-1] + ['|'.join(data[-1])] + [gene_id in target_dict, gene_id in promoter_dict])
+    #             ) + '\n'
+    #         )
