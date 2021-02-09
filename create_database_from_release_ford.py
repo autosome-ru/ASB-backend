dafd774de7da011bb4e28cdf06338a050baa7de4
Sashok <boytsovs@mail.ru>
2021-02-09 20:16:23 +0300

    fix adastra fdr filter

--- create_database_from_release_ford.py
+++ create_database_from_release_ford.py
@@ -350,7 +350,7 @@ if __name__ == '__main__':
                             assert other_id == {'TF': another_dict.get(cl_dict_reverse.get(parameter.get('CL'))),
                                                 'CL': another_dict.get(parameter.get('TF'))}[param]
                             assert exp_snp.ref_readcount == parameter['ref_counts']
-                            assert round(exp_snp.p_value_alt, 6) == round(parameter['alt_pvalues'], 6)
+                            assert round(exp_snp.p_value_alt, 5) == round(parameter['alt_pvalues'], 5)
                             assert exp_snp.bad == conv_bad[parameter['BAD']]
 
                         exp_snps.append(exp_snp)
