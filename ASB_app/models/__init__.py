4cee952db25d79af3390dd0ce7ac1a8af5a95b77
Sashok <boytsovs@mail.ru>
2021-08-24 19:27:51 +0300

    release zanthar

--- ASB_app/models/__init__.py
+++ ASB_app/models/__init__.py
@@ -8,5 +8,6 @@ for release in Release.__subclasses__():
     release.db.create_all(release.name)
     release.session.commit()
 
-current_release.db.create_all('candidates')
+current_release.db.create_all('candidates_susan')
+current_release.db.create_all('candidates_zanthar')
 current_release.db.create_all('tickets')
