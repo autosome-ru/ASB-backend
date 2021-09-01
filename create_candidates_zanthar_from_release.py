79b793cdb0f1b08a77c7b5ae9091102e2ec2af52
Sashok <boytsovs@mail.ru>
2021-09-02 01:23:56 +0300

    correct release path

--- create_candidates_zanthar_from_release.py
+++ create_candidates_zanthar_from_release.py
@@ -50,7 +50,7 @@ conv_bad = dict(zip(
 ))
 
 if __name__ == '__main__':
-    with open(os.path.join(release_path, 'release_stats', 'convert_cl_names.json')) as file:
+    with open(os.path.join(release_path, 'release_stats', 'convert_cell_lines.json')) as file:
         cl_dict = json.loads(file.readline())
 
     cl_dict_reverse = {}
