08c603e4b3949beb2be7b3418a660e07c4f958de
Sergey Abramov <aswq22013@gmail.com>
2022-03-15 10:51:53 +0300

    create dirs

--- ASB_app/__init__.py
+++ ASB_app/__init__.py
@@ -82,7 +82,7 @@ from . import routes
 from . import utils
 from . import scheduler_jobs
 
-from service.ananastra_service import get_tickets_dir
+from ASB_app.service.ananastra_service import get_tickets_dir
 
 for suffix in ('', 'accepted', 'logs'):
     check_and_create_dir(get_tickets_dir(suffix=suffix))
