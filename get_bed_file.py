63ab2955007e1dfc1f19edbf60712313a2f27812
m1kss <boytsovs.av@phystech.edu>
2022-05-25 19:40:25 -0400

    get bedfile path

--- get_bed_file.py
+++ get_bed_file.py
@@ -4,7 +4,7 @@ from ASB_app.releases import current_release
 S = current_release.SNP
 session = current_release.session
 
-with open('D:\\Sashok\\snps_{}.bed'.format(current_release.name), 'w', encoding='utf-8') as f:
+with open(os.path.expanduser('~/adastra/snps_{}.bed').format(current_release.name), 'w', encoding='utf-8') as f:
     for s in S.query:
         assert s.fdr_class and s.fdr_class != '1'
         f.write('\t'.join(map(str, [s.chromosome, s.position - 1, s.position, 'rs' + str(s.rs_id)])) + '\n')
