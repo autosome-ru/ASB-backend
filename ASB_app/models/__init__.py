593973d25386fb825c79251273fd725e99fc43e7
m1kss <boytsovs.av@phystech.edu>
2022-05-29 11:54:57 -0400

    candidates release key in models init

--- ASB_app/models/__init__.py
+++ ASB_app/models/__init__.py
@@ -8,5 +8,5 @@ for release in Release.__subclasses__():
     release.db.create_all(release.name)
     release.session.commit()
 
-current_release.db.create_all('candidates_zanthar')
+current_release.db.create_all(cnadidates_release_key)
 current_release.db.create_all('tickets')
