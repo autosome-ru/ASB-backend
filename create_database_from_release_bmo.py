4ad08edf25392feb26880ec59e403b029358a0a2
wishabc <aswq22013@gmail.com>
2023-05-06 18:39:00 -0700

    added database

--- create_database_from_release_bmo.py
+++ create_database_from_release_bmo.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
             try:
                 key, value = line.strip().split()
             except:
-                print(line, key, value)
+                print(line)
             cl_dict_reverse[value] = key
             cl_dict[key] = value
 
