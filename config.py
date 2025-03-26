736365b769d8f8e08ca29b201b7a55d45e826e90
wishabc <aswq22013@gmail.com>
2025-03-26 18:42:16 +0800

    removed releases except mabel

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ dir = pathlib.Path(__file__).parent.absolute()
 
 class Config(object):
     SQLALCHEMY_BINDS = {
-        'soos': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_soos?charset=utf8',
+        #'soos': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_soos?charset=utf8',
         # 'susan': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_susan?charset=utf8',
         # 'zanthar': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_zanthar?charset=utf8',
         # 'candidates_zanthar': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_candidates_zanthar?charset=utf8',
-        'candidates_billcipher': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_candidates_billcipher?charset=utf8',
-        'billcipher': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_billcipher?charset=utf8',
+        #'candidates_billcipher': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_candidates_billcipher?charset=utf8',
+        #'billcipher': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_billcipher?charset=utf8',
         'candidates_mabel': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_candidates_billcipher?charset=utf8',
         'mabel': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_billcipher_fixed_motifs?charset=utf8',
         'tickets': 'mysql://adastra:pass=ADASTRA880@localhost:3306/adastra_tickets?charset=utf8',
